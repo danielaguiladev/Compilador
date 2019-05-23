0a6cd10860ccced9847543b695c1aab3cc9614cc
Daniel Aguila <daniel.aguila@localiza.com>
2019-05-23 14:35:25 -0300

    init

---
+++ src/compilador/Compilador.java
@@ -0,0 +1,16 @@
+package compilador;
+
+/**
+ *
+ * @author 132683
+ */
+
+public class Compilador {
+    public static TS tabelaSimbolos;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Lexer lexer = new Lexer(tabelaSimbolos, "hellojavinha.jvn");
+        Parser parser = new Parser(lexer);
+        parser.nonRecursiveParser();
+    }
+}
---
+++ src/compilador/Tag.java
@@ -0,0 +1,61 @@
+package compilador;
+
+/**
+ *
+ * @author 132683
+ */
+public enum Tag {
+    
+    // fim de arquivo
+    EOF,
+    
+    //Operadores
+    RELOP_LT,       // <
+    RELOP_OR,       //||
+    RELOP_AND,      //&&
+    RELOP_LE,       // <=
+    RELOP_GT,       // >
+    RELOP_GE,       // >=
+    RELOP_EQ,       // ==
+    RELOP_NE,       // !=
+    RELOP_ASSIGN,   // =
+    RELOP_SUM,      // +
+    RELOP_MINUS,    // -
+    RELOP_MULT,     // *
+    RELOP_DIV,      // /
+    RELOP_UNNE,     // -
+    RELOP_UNESC,    //!
+    
+    //Simbolos
+    SMB_OP,         // (
+    SMB_CP,         // )
+    SMB_OB,         // {
+    SMB_CB,         // }
+    SMB_COMMA,      // ,
+    SMB_SEMICOLON,  // ;
+    SMB_NULL,       // zerar auxnegativo
+    
+    //identificador
+    ID,
+
+
+    //numeros
+    ConstInteira,
+    INTEGER,
+    FLOAT,
+    ConstReal,
+    
+    //String em aspas simples
+    //strings
+    char_const,
+	
+    
+    //String em aspas duplas
+    STRING,
+    ConstString,
+    
+    // palavra reservada
+    KW,
+    
+      
+}
